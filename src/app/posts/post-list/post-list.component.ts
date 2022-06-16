91cc71fc341e946da184f37213dc2800f37e6610
brikbrown <brianna.kbrown@yahoo.com>
2022-06-16 19:44:01 -0400

    add backend using express

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -8,33 +8,31 @@ import { PostDeleteComponent } from './post-delete/post-delete.component';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+  styleUrls: ['./post-list.component.scss'],
 })
 export class PostListComponent implements OnInit {
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
- posts: Post[] = [];
- private postsSub: Subscription;
+  constructor(private ps: PostService, private dl: MatDialog) {}
 
-constructor(private ps: PostService,
-            private dl: MatDialog) {
-}
+  ngOnInit(): void {
+    this.posts = this.ps.getPosts();
+    this.postsSub = this.ps
+      .getPostUpdatedListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
 
-ngOnInit(): void {
-  this.posts = this.ps.getPosts();
-  this.postsSub = this.ps.getPostUpdatedListener().subscribe((posts: Post[]) => {
-    this.posts = posts;
-  });
-}
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
+  }
 
-ngOnDestroy(): void {
-  this.postsSub.unsubscribe();
-}
-
-confirmDelete(i: number): void {
-  const dialogRef = this.dl.open(PostDeleteComponent, {
-    width: '100px',
-    data: i
-  })
-}
-
+  confirmDelete(i: number): void {
+    const dialogRef = this.dl.open(PostDeleteComponent, {
+      width: '250px',
+      data: i,
+    });
+  }
 }
